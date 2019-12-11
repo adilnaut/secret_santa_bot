4dc58029ba3d98374d7f61c62aadae2698f8b52e
Adilet <adilnaut@gmail.com>
2019-12-11 14:05:47 +0600

    first santa

--- app.py
+++ app.py
@@ -28,9 +28,9 @@ def edit_json(value):
     # print(r.json())
 def get_json():
     url = "https://api.jsonbin.io/b/5df0a111bc5ffd0400977abc"
-    headers = {'secret-key': '$2b$10$R248o3.U5BEnIlPpNDFs.uERh/ui3h4oD/xLYz6Cbi2DTr.RrM52y'}
+    headers = {'secret-key': '$2b$10$R248o3.U5BEnIlPpNDFs.uERh/ui3h4oD/xLYz6Cbi2DTr.RrM52y', 'Content-Type': 'application/json'}
 
-    req = requests.put(url, json=None, headers=headers)
+    req = requests.put(url, headers=headers)
     print(req.text)
     # r = requests.get("https://api.myjson.com/bins/1aneis")
     # print(r.json())
