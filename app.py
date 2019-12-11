4f8df0a95fe340a0580f3ba98552d8085620fdbb
Adilet <adilnaut@gmail.com>
2019-12-11 14:10:30 +0600

    first santa

--- app.py
+++ app.py
@@ -19,7 +19,7 @@ botName = "easy_santa_astana_bot" #Without @
 def edit_json(value):
     url = "https://api.jsonbin.io/b/5df0a111bc5ffd0400977abc"
     # url = 'https://api.jsonbin.io/b/<BIN_ID>'
-    headers = {'Content-Type': 'application/json'}
+    headers = {'Content-Type': 'application/json', 'secret-key': '$2b$10$R248o3.U5BEnIlPpNDFs.uERh/ui3h4oD/xLYz6Cbi2DTr.RrM52y',}
     data = {"data": value}
 
     req = requests.put(url, json=data, headers=headers)
